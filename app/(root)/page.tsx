9763878289337da11b48ac783c6ac441a3705642
Anushka404 <anushkatewari2004@gmail.com>
2025-02-20 17:36:34 +0530

    cards part1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import { createHistogram } from "perf_hooks";
+import { title } from "process";
 
 export default async function Home({searchParams}:{
   searchParams: Promise<{query?: string}> //searchParams is a promise that resolves to an object with a query property
 }) {
   //extract query
   const query = (await searchParams).query;
+  const posts= [{
+    _createdAt: 'Yesterday',
+    views:55,
+    author:{_id:1},
+    _id: 1,
+    description: 'This is a description',
+    image:'https://via.placeholder.com/150',
+    category:"Robots",
+    title: 'Robotics Startup',
+  }];
+
   return (
     <>
     <section className="pink_container">
@@ -18,6 +31,14 @@ export default async function Home({searchParams}:{
     <SearchForm query={query}/>
     </section>
     
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query? `Search results for ${query}`: "All Startups"}
+      </p>
+      <ul className="mt-7 card_grid">
+
+      </ul>
+    </section>
     </>
   );
 }

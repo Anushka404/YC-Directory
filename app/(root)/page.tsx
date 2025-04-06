2e2703548bff30bc242f0e6cbeb29ef7de77441b
Anushka404 <anushkatewari2004@gmail.com>
2025-04-06 19:50:21 +0530

    startup card done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from '../../components/StartupCard';
 import { createHistogram } from "perf_hooks";
 import { title } from "process";
 
@@ -9,12 +10,12 @@ export default async function Home({searchParams}:{
   //extract query
   const query = (await searchParams).query;
   const posts= [{
-    _createdAt: 'Yesterday',
+    _createdAt: new Date(),
     views:55,
-    author:{_id:1},
+    author:{_id:1, name:'John Doe'},
     _id: 1,
     description: 'This is a description',
-    image:'https://via.placeholder.com/150',
+    image:'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     category:"Robots",
     title: 'Robotics Startup',
   }];
@@ -36,7 +37,13 @@ export default async function Home({searchParams}:{
         {query? `Search results for ${query}`: "All Startups"}
       </p>
       <ul className="mt-7 card_grid">
-
+        {posts?.length > 0 ?(
+          posts.map((post:StartupCardType, index:number) => (
+            <StartupCard key={post._id} post={post} />
+          ))
+        ) : (
+          <p className="no-results">No startups found</p>
+        )}
       </ul>
     </section>
     </>

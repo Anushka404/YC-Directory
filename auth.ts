aff6c98b3db0514aa1967413b83dfb6f209b210d
Anushka404 <anushkatewari2004@gmail.com>
2025-04-15 00:36:48 +0530

    author authentication fix

--- auth.ts
+++ auth.ts
@@ -45,6 +45,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     async session({session, token}){
       Object.assign(session, source: { id:token.id });
       return session;
+      
     }
   },
 });
